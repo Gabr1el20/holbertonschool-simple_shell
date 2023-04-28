c88e3a70a57a5fbbe7a0d3db9a7d9fe90edf779b
Gabr1el20 <6327@holbertonstudents.com>
2023-04-28 07:26:10 -0700

    Handled spaces.2

--- shell.c
+++ shell.c
@@ -7,7 +7,7 @@
 */
 int main(int ac __attribute__((unused)), char *av[])
 {
-	char *comando = NULL, **argus, *completo = NULL;
+	char *comando = NULL, **argus, *com = NULL;
 	size_t bufsize = 0;
 	ssize_t buffer = 0;
 	int status = 0, count = 0;
@@ -22,23 +22,25 @@ int main(int ac __attribute__((unused)), char *av[])
 			free(comando), exit(EXIT_SUCCESS);
 		if (comando[buffer - 1] == '\n')
 			comando[buffer - 1] = '\0';
+		if (strcmp(comando, "exit") == 0)
+			free(comando), exit(0);
 		if (strlen(comando) == 1)
 			continue;
 		if (checkemptiness(comando) == 1)
-			status = 0, break;
+		{
+			status = 0;
+			break;
+		}
 		argus = splitter(comando);
 		if (argus && argus[0])
 		{
 			if (stat(argus[0], &st) == 0)
-				completo = argus[0], status = exq(completo, argus);
+				com = argus[0], status = exq(com, argus);
 			else if (stat(argus[0], &st) == -1)
 			{
-				completo = get_path(argus[0]);
-				if (completo)
-				{
-					status = exq(completo, argus), free(completo);
-					(status == -1) ? status = 127 : 0;
-				}
+				com = get_path(argus[0]);
+				if (com)
+					status = exq(com, argus), free(com), (status == -1) ? status = 127 : 0;
 				else
 					_perror(av[0], count, comando);
 			}
